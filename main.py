217afd58e8ea84f21d6b4c029400cfdab6348e34
joe.byrne1 <joe.byrne@and.digital>
2024-04-18 10:25:29 +0100

    defining class for contact

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('main running')
+
+
+class Contact:
+    def __init__(self, first_name, last_name, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+
+
+address_book = []
